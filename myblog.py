220d5325d9a39deb510c5f9e42ef86c6c3b92760
gevannmullins <21a32b43c>
2017-01-16 16:30:03 +0200

    Added a mainPage/homePage. Fixed signupPage functionality and styling. LoginPage Styling fixed, still issues with login functionality. Single blog links are working, still issues with the template.

--- myblog.py
+++ myblog.py
@@ -1,21 +1,25 @@
 import os
-import re
-from string import letters
-import webapp2
 import jinja2
+import webapp2
+import cgi
+import re
+import datetime
+import random
+import hashlib
+import hmac
+from string import letters
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
-
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-
 class BlogHandler(webapp2.RequestHandler):
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -25,39 +29,56 @@ class BlogHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+    #store cookies
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
 
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
 
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+    def render_post(response, post):
+        response.out.write('<b>' + post.subject + '</b><br>')
+        response.out.write(post.content)
 
 class MainPage(BlogHandler):
     def get(self):
-        self.render('front.html')
-
+        self.render('main_page.html')
 
 ##### blog stuff
 
-def blog_key(name='default'):
+def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
-
 class Post(db.Model):
-    subject = db.StringProperty(required=True)
-    content = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p=self)
-
+        return render_str("post.html", p = self)
 
 class BlogFront(BlogHandler):
     def get(self):
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
-        self.render('front.html', posts=posts)
-
+        self.render('front.html', posts = posts)
+        self.write(posts)
 
 class PostPage(BlogHandler):
     def get(self, post_id):
@@ -68,8 +89,7 @@ class PostPage(BlogHandler):
             self.error(404)
             return
 
-        self.render("permalink.html", post=post)
-
+        self.render("permalink.html", post = post)
 
 class NewPost(BlogHandler):
     def get(self):
@@ -80,13 +100,12 @@ class NewPost(BlogHandler):
         content = self.request.get('content')
 
         if subject and content:
-            p = Post(parent=blog_key(), subject=subject, content=content)
+            p = Post(parent = blog_key(), subject = subject, content = content)
             p.put()
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
             error = "subject and content, please!"
             self.render("newpost.html", subject=subject, content=content, error=error)
-
 
 ###### Unit 2 HW's
 class Rot13(BlogHandler):
@@ -99,31 +118,74 @@ class Rot13(BlogHandler):
         if text:
             rot13 = text.encode('rot13')
 
-        self.render('rot13-form.html', text=rot13)
-
+        self.render('rot13-form.html', text = rot13)
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-
-
 def valid_username(username):
     return username and USER_RE.match(username)
 
-
 PASS_RE = re.compile(r"^.{3,20}$")
-
-
 def valid_password(password):
     return password and PASS_RE.match(password)
 
-
-EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-
-
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
+#
+# Database Entities
+#
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    password = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def get_all(cls, sortby='name'):
+        return User.all().order(sortby)
+
+    @classmethod
+    def get_by_name(cls, name):
+        return User.all().filter('name =', name).get()
+
+    @classmethod
+    def add(cls, name, pw, email=None):
+        u = User(name = name, password = pw, email = email)
+        u.put()
+        return u
+
+
+class LoginHandler(BlogHandler):
+    def write_error(self):
+        self.write(
+            'login-form.html',
+            error_login="Invalid Login"
+        )
+
+    def get(self):
+        # self.write('login-form.html')
+        self.render('login-form.html')
+
+    def post(self):
+        # validate credentials
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        if username and password:
+            rs = User.get_by_name(username)
+
+            # if rs and valid_password(username, password, rs.password):
+            if rs:
+                self.set_secure_cookie('user_id', str(rs.key().id()))
+                self.redirect('/')
+            else:
+                self.write_error()
+        else:
+            self.write_error()
+
 
 class Signup(BlogHandler):
+
     def get(self):
         self.render("signup-form.html")
 
@@ -134,8 +196,7 @@ class Signup(BlogHandler):
         verify = self.request.get('verify')
         email = self.request.get('email')
 
-        params = dict(username=username,
-                      email=email)
+        params = dict(username = username, email = email)
 
         if not valid_username(username):
             params['error_username'] = "That's not a valid username."
@@ -155,27 +216,30 @@ class Signup(BlogHandler):
         if have_error:
             self.render('signup-form.html', **params)
         else:
-            self.redirect('/unit2/welcome?username=' + username)
+            User.add(username, password, email)
+            self.redirect('/welcome?username=' + username)
 
+class LogoutHandler(BlogHandler):
+    def get(self):
+        self.logout()
+        self.redirect('/')
 
 class Welcome(BlogHandler):
     def get(self):
         username = self.request.get('username')
         if valid_username(username):
-            self.render('welcome.html', username=username)
+            self.render('welcome.html', username = username)
         else:
             self.redirect('/signup')
-
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/rot13', Rot13),
                                ('/signup', Signup),
                                ('/welcome', Welcome),
-                               # ('/unit2/rot13', Rot13),
-                               # ('/unit2/signup', Signup),
-                               # ('/unit2/welcome', Welcome),
                                ('/blog/?', BlogFront),
                                ('/blog/([0-9]+)', PostPage),
-                               ('/blog/newpost', NewPost),
+                               ('/newpost', NewPost),
+                               ('/login', LoginHandler),
+                               ('/logout', LogoutHandler),
                                ],
                               debug=True)
